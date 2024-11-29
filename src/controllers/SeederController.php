cb706ac76607f65df6d745c610dd23cc8625fa08
Maxim Statkevich <maks@mail.com>
2024-11-29 13:25:18 +0100

    fix seeder file

--- src/controllers/SeederController.php
+++ src/controllers/SeederController.php
@@ -8,12 +8,6 @@ class SeederController
 {
     public function runSeeder(): void
     {
-        if ($_SERVER['SERVER_NAME'] !== 'localhost') {
-            http_response_code(403);
-            echo json_encode(['error' => 'Seeder can only be run in local environment']);
-            return;
-        }
-
         // Запускаем сидер
         $seeder = new EmployeeSeeder();
         $seeder->run(10000);
